b4c902c358b727db8897eba276353f1b5c48bc3d
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-02 13:30:53 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char sr[50];
+	int i,s=0;
+  clrscr();
+	printf("enter the sentence:");
+	scanf("%[^\n]s",sr);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(sr[i]==' ')
+		{
+			s=s+1;
+		}
+	}
+	printf("\nno of white spaces=%d",s);
+  getch();
+}
