38e73956f6862b4643babf184cd385c34f616e9b
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-05 16:10:20 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int i,n,a[10];
+clrscr();
+printf("Enter the name");
+scanf("%s",&a[n]);
+printf("how mony test u have");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+printf("%s",a[n]);
+getch ();
+return 0;
+}
