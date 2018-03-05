5400997d79876f81a0b288ca35c618a2f05db249
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-05 16:19:42 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int i,j,n,a[10],b[10];
+clrscr();
+printf("Enter the name");
+scanf("%s",&a[0]);
+printf("enter the second letter");
+scanf("%s",&b[0]);
+printf("%s%s",a[0],b[0]);
+getch();
+return 0;
+}
