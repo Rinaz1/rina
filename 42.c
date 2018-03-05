76055a2e5d41dbf2009c9afd2854691b97c52483
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-05 16:16:23 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i,j,n,a[10],b[10];
+clrscr();
+printf("Enter the name");
+scanf("%s",&a[0]);
+printf("enter the second letter");
+scanf("%s",&b[0]);
+if(a[i]<=b[j])
+printf("%s",a[0]);
+else
+printf("%s",b[0]);
+getch();
+return 0;
+}
