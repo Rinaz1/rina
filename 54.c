2b0542771d90a7ff91c339763a9d83d5125848e7
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-12 16:35:21 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,b,d,i;
+printf("enter the no");
+scanf("%d",&n);
+for(i=n;i=0;i--)
+{
+{
+d=n-1;
+b=n/2;
+}
+if(b==0)
+printf("%d",b);
+}
+getch();
+}
