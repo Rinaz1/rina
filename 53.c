a3d6d5cb293b066eb162e081ae76986cb6901d33
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-12 16:16:50 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,b,sum=0,d,i;
+printf("enter the no");
+scanf("%d",&n);
+for(i=n;i=0;i++)
+{
+d=n\10;
+b=n%10;
+sum=sum+b;
+d=n;
+}
+getch();
+}
