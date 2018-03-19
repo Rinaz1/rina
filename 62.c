cc14312db3aa69feae8a71ce01870d0e079477ad
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-03-19 03:38:20 -0700

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,24 @@
+
+int main() 
+{
+	char a[100];
+	int n,i,num,count=0;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]=='0'||a[i]=='1')
+	    {   
+	        count++;
+	    }
+	    else
+	    {
+	       printf("no..");
+	       break;
+	    }
+	}
+	if(n==count)
+	{
+	     printf("yes...it is in binary representation");
+	}
+}
